204862e2c633f7a3e40371fbd136fd30ac5cfb3a
mzati0 <mzatichikoko@gmail.com>
2025-04-06 18:43:58 +0200

    Created Prefabs and Prototype tile maps

--- Assets/RuriScript.cs
+++ Assets/RuriScript.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
-    // heyooo, anything in double slashes are comments, they are not read by the computer, they are just for you to read
+    // hey bro's, anything in double slashes are comments, they are not read by the computer, they are just for you to read
     // so you can understand me code good sirs, I need to practice coding for others to understand.
     
     private Transform _playerTransform; //this is a variable that stores the transform of the player
@@ -15,6 +15,13 @@ public class PlayerScript : MonoBehaviour
     
     private InputAction _moveAction; //this is a variable that stores the move action of the player
     private InputAction _switchAction;  //unused for now
+    
+    public GameObject ottoPrefab; //this is a variable that stores the otto game object
+    private GameObject _otto; //this is a variable that stores the otto game object, this is the one that will be used in the game
+    public bool hasOtto = false; //this is a variable that stores if the player has otto or not
+    //it's public because this is also checked when the game starts, so you can set it in the inspector
+    
+    public float movementSpeed = 5f; //this is a variable that stores the movement speed of the player, this is the speed at which the player moves
     
     public enum Direction { Up, Down, Left, Right }
     public static Direction CurrentDirection = Direction.Down;
@@ -28,6 +35,23 @@ public class PlayerScript : MonoBehaviour
         _spriteRenderer = this.GetComponent<SpriteRenderer>();
         _spriteRenderer.sprite = _spriteRenderer.sprite;
         _audioSource = this.GetComponent<AudioSource>();
+        if (hasOtto == true)
+        {
+         addOtto();
+        }
+
+    }
+
+    private void addOtto()
+    {
+        _otto = Instantiate(ottoPrefab, _playerTransform); //creates a new game object
+        _otto.transform.localPosition = new Vector3(0, 0.43f, 0); //this is the position of the otto game object relative to the player
+        _otto.transform.localRotation = Quaternion.identity; //Identity just means zero rotation essentially.
+    }
+    private void removeOtto()
+    {
+        Destroy(_otto); //destroys the otto game object
+        _otto = null; //sets the otto game object to null, basically like when we instantiated it
     }
 
     private void changeSprite(String path)
@@ -52,7 +76,7 @@ public class PlayerScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //start isn't used because we using awake instead
+        //start isn't used because we're using awake instead
     }
  private void OnAttack(InputAction.CallbackContext context)
  {
@@ -62,7 +86,7 @@ public class PlayerScript : MonoBehaviour
     {
         //move the player
         Vector2 moveDir = _moveAction.ReadValue<Vector2>();
-        _playerTransform.position += new Vector3(moveDir.x, moveDir.y, 0) * Time.deltaTime;
+        _playerTransform.position += (new Vector3(moveDir.x, moveDir.y, 0) * Time.deltaTime)* movementSpeed;
         
         //update the sprite
         if (moveDir.x != 0 || moveDir.y != 0){ //if the player is moving
