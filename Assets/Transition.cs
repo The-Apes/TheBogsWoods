a82a8cf2182e4de120ab4b15894fc5a698d17466
Mzati Chikoko <mzatichikoko@gmail.com>
2025-04-07 03:33:20 +0200

    Update Transition.cs

--- Assets/Transition.cs
+++ Assets/Transition.cs
@@ -5,10 +5,6 @@ public class Transition : MonoBehaviour
     public Vector3 sceneLocation= new Vector3(0, 1, -10); // The location to move the camera to when the player enters the trigger
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Camera mainCamera; // The camera to move
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
