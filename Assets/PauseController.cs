1f6ce1f20324e18ec18ee4144d4422d8af49665c
phiwebhengu <bhengu080104@gmail.com>
2025-04-14 11:10:50 +0200

    Fixed some Pausing issues
    When exiting to main menu from pause screen in game and then starting the game again from main menu, the game would stay in frozen/paused state. Fixed it

--- Assets/PauseController.cs
+++ Assets/PauseController.cs
@@ -2,12 +2,19 @@ using UnityEngine;
 
 public class PauseController : MonoBehaviour
 {
-    public static bool IsGamePaused {get; private set;} = false;
+    public static bool IsGamePaused { get; private set; } = false;
+
+    private void Start()
+    {
+        // Ensure the game is not paused when it starts
+        IsGamePaused = false;
+        Time.timeScale = 1;
+    }
 
     public static void SetPause(bool pause)
     {
         IsGamePaused = pause;
         Time.timeScale = pause ? 0 : 1; // Pauses or unpauses the game
     }
-    
 }
+
