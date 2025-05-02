1f6c9be95078319c44b301b394d6eb4736b85489
Phiwe Bhengu <bhengu080104@gmail.com>
2025-05-02 08:43:56 +0200

    More VScode shit

--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebc093069ecc5af87d43a4cefafd3be43ce05c38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a6e752701baa09d667c6eafae85ab7ced97e282")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
