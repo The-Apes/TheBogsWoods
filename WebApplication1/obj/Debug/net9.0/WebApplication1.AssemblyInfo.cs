8fbe5961d8e780063c89068a9bb6551466d688d0
Phiwe Bhengu <bhengu080104@gmail.com>
2025-05-12 20:01:43 +0200

    ignore

--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f6c9be95078319c44b301b394d6eb4736b85489")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ac4945d4bd915349aa7d38df1b8199a82abdbb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
