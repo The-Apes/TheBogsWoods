69c2b9414d3f7c424879cd30cab89817e241315b
Phiwe Bhengu <bhengu080104@gmail.com>
2025-05-19 20:22:29 +0200

    Update GlobalHelper.cs

--- Assets/Scripts/GlobalHelper.cs
+++ Assets/Scripts/GlobalHelper.cs
@@ -7,3 +7,5 @@ public static class GlobalHelper
         return $"{obj.scene.name}_{obj.transform.position.x}_{obj.transform.position.y}"; //Chest_3_4
     }
 }
+//Ignore this file, it is just a helper class to generate unique IDs for objects in the scene I was using for testing purposes
+// and will be removed in the final version of the game.
