c2948e3e327e3e167657242532d5f8d0a3aeed00
mzati0 <mzatichikoko@gmail.com>
2025-06-10 09:39:04 +0200

    "What is this place" Dialogue

--- Assets/Scripts/OneTimeDialogueTrigger.cs
+++ Assets/Scripts/OneTimeDialogueTrigger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class OneTimeDialogueTrigger : MonoBehaviour
 {
    public DialogueAsset dialogue; 
-   private void OnCollisionEnter2D(Collision2D other)
+   private void OnTriggerEnter2D(Collider2D other)
    {
       if (!other.gameObject.CompareTag("Player")) return;
       DialogueManager.instance.StartDialogue(dialogue);
